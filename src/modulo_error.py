518e371e0bb1aacada7a02e4713c6dde954168ea
lara <saeti@elsweyr.(none)>
2013-05-13 00:51:56 +0100

    programa update

--- src/modulo_error.py
+++ src/modulo_error.py
@@ -1,16 +1,15 @@
-# modulo error
-
+# El error obtenido usando el metodo del trapecio
 import sys
 from math import *
 
 def error(valor_trapecio):
-  valor_real=2/float(pi)
+  valor_real=2/pi
   return abs(valor_real-valor_trapecio)
 
 if __name__=='__main__':
-  if len(sys.argv)==5:
+  if len(sys.argv)==2:
     valor_trapecio=float(sys.argv[1])
     
     print error(valor_trapecio)
   else:
-    print "el modo de uso es:%s valor con metodo de trapecio" % (sys.argv[0])
+    print "El modo de uso es:%s <valor con metodo de trapecio>" % (sys.argv[0])
--- src/modulo_trapecio.py
+++ src/modulo_trapecio.py
@@ -4,20 +4,19 @@ import sys
 from math import *
 
 def trapecio_senxpi(n):
-  a=-2
-  b=-1
-  h=(b-a)/float(n)
-  s=(sin(a*pi)+sin(b*pi))/float(2)
+  a=-2.0
+  b=-1.0
+  h=(b-a)/n
+  s=(sin(a*pi)+sin(b*pi))/2.0
   for i in range(1,n):
     s+=sin(pi*(a+i*h))
   return h*s
 
 if __name__=='__main__':
   if len(sys.argv)==2:
-      n=int(sys.argv[1])	# n es el numero de trapecio deseado
+      n=int(sys.argv[1])
       
       print trapecio_senxpi(n)    
   else:
-    print "el modo de uso es:%s numero de trapecio" % (sys.argv[0])
-     
+    print "El modo de uso es:%s <numero de trapecio>" % (sys.argv[0])
     
